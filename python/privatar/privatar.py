5fb45d1cc00f0cbc8f4e54a8552914c9d3cdd6b7
Edmund von der Burg <evdb@mysociety.org>
2011-03-21 21:08:33 +0000

    got url creation working

--- python/privatar/privatar.py
+++ python/privatar/privatar.py
@@ -1,7 +1,77 @@
-
+from Crypto.Hash import MD5
+import urlencoding # pip install -e 'git://github.com/nshah/python-urlencoding.git#egg=urlencoding'
 
 class privatar():
+    http_base  = 'http://www.privatar.org'
+    https_base = 'https://privatar-org.appspot.com'
 
-    def xor_md5s (self, md5a, md5b ) :
+
+    def __init__(self, site_key, shared_secret):
+        self.site_key      = site_key
+        self.shared_secret = shared_secret    
+        
+    @classmethod
+    def xor_md5s (cls, md5a, md5b ) :
         """Return the XOR result of the inputs"""
         return "%032x" % ( int(md5a, 16) ^ int(md5b, 16) )
+
+    @classmethod
+    def md5(cls, *input):        
+        return MD5.new( '-'.join(input) ).hexdigest();
+    
+    def url( self, email=False, email_md5=False, secure=False, salt=False, query=False, suffix=False ):
+        
+        # get the email_md5 to use
+        if email:
+            email_md5 = self.md5( email )
+        if not email_md5:
+            raise Exception("Require either 'email' or 'email_md5'")      
+        
+        if not salt:
+            salt = self.generate_salt( email_md5 )
+        
+        avatar_code = self.generate_avatar_code( email_md5, salt )
+        
+        if secure:
+            base = self.https_base
+        else:
+            base = self.http_base
+        
+        url = "%s/avatar/%s" % ( base, avatar_code )
+
+        if query:
+            url += '?' + urlencoding.compose_qs( query, sort=True )
+
+        if suffix:
+            url += '.' + suffix
+
+        return url
+    
+    def generate_salt( self, email_md5 ):
+
+        # create the md5 hash
+        hash = self.md5( self.shared_secret, email_md5 )
+        number = int(hash[:16], 16)
+
+        alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+        base36 = ''
+        while number:
+            number, i = divmod(number, 36)
+            base36 = alphabet[i] + base36
+
+        # keep it short
+        return "%08s" % base36[:8]
+        
+    def generate_avatar_code( self, email_md5, salt ):
+        # create the hash to encrypt the email_md5 with
+        one_time_pad = self.md5( salt, self.shared_secret )
+        
+        # encrypt the email_md5
+        encrypted_md5 = self.xor_md5s( email_md5, one_time_pad )
+        
+        # get the first letter of the shared_secret
+        first_letter = self.shared_secret[:1]
+        
+        return '-'.join( [ self.site_key, salt, first_letter, encrypted_md5 ] )
+        
--- python/privatar/test.py
+++ python/privatar/test.py
@@ -1,6 +1,6 @@
 """Unit test for privatar.py"""
 
-import privatar
+from privatar import privatar
 import unittest
 
 import json
@@ -9,7 +9,6 @@ import json
 class XORValues(unittest.TestCase):                          
 
     known_values = json.loads( open('test_data/xor_samples.json').read() )
-    p = privatar.privatar()
 
     def test_xor_samples(self):                          
         """test xor using known values and check it roundtrips correctly"""
@@ -19,11 +18,37 @@ class XORValues(unittest.TestCase):
             out_md5 = test['out']
             key     = test['key']
 
-            result = self.p.xor_md5s(key, in_md5)                    
+            result = privatar.xor_md5s(key, in_md5)                    
             self.assertEqual(out_md5, result)                  
 
-            new_output = self.p.xor_md5s(key, out_md5)                    
-            self.assertEqual(in_md5, new_output)                  
+            new_output = privatar.xor_md5s(key, out_md5)                    
+            self.assertEqual(in_md5, new_output)
+
+class URLCreation(unittest.TestCase):
+
+    test_data = json.loads( open('test_data/url_creation.json').read(), encoding='ASCII' )
+    _privatar = False
+
+    def privatar(self):
+        if not self._privatar:
+            config = self.test_data['config']
+            self._privatar = privatar( site_key=config['site_key'], shared_secret=config['shared_secret'] )
+        return self._privatar
+     
+
+    def test_url_creation(self):
+        """test that urls are generated as expected"""
+        for test in self.test_data['tests']:
+            u_args = test['args']
+
+            # convert keys to ascii because python gets all tarty
+            args = dict((k.encode('ascii'), v) for (k, v) in u_args.items())
+
+            url = self.privatar().url( **args );
+            self.assertEqual( url, test['urls']['http'] )
+
+            secure_url = self.privatar().url( secure=True, **args );
+            self.assertEqual( secure_url, test['urls']['https'] )                    
 
 if __name__ == "__main__":
     unittest.main()   
