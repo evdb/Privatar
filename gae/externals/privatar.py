b848cc76d09ba983a91dc459da9dfdbcafd65c53
Edmund von der Burg <evdb@mysociety.org>
2011-03-23 22:28:34 +0000

    symlink privatar.py in to gae project

---
+++ gae/externals/privatar.py
@@ -0,0 +1,1 @@
+../../python/privatar/privatar.py
--- gae/handlers/avatar.py
+++ gae/handlers/avatar.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+from externals.privatar import privatar
 
 class AvatarHandler(webapp.RequestHandler):
     def get(self):
