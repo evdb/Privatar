0d89aab6036eedc71736d71a064636ed2d6b6746
Edmund von der Burg <evdb@mysociety.org>
2011-03-23 21:47:15 +0000

    Laid out code for GAE

--- gae/main.py
+++ gae/main.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
-
-class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+from handlers.index  import IndexHandler
+from handlers.avatar import AvatarHandler
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
-                                         debug=True)
+
+    # determine if this is the dev server or not
+    try:
+      is_dev = os.environ['SERVER_SOFTWARE'].startswith('Dev')
+    except:
+      is_dev = False
+
+    application = webapp.WSGIApplication(
+        [
+            ( '/avatar/.*', AvatarHandler ),
+            ( '/.*',        IndexHandler  ),
+        ],
+        debug=is_dev # olny debug on dev server
+    )
     util.run_wsgi_app(application)
 
 
