fa628424465897afec5b285cc491a2b9b8938a22
Edmund von der Burg <evdb@mysociety.org>
2011-03-27 13:05:59 +0100

    Add logging at appropriate level

--- gae/main.py
+++ gae/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+import logging
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
@@ -13,8 +15,11 @@ def main():
     # determine if this is the dev server or not
     try:
       is_dev = os.environ['SERVER_SOFTWARE'].startswith('Dev')
+      logging.getLogger().setLevel(logging.DEBUG)
     except:
       is_dev = False
+      logging.getLogger().setLevel(logging.WARN)
+
 
     application = webapp.WSGIApplication(
         [
@@ -23,6 +28,7 @@ def main():
         ],
         debug=is_dev # olny debug on dev server
     )
+
     util.run_wsgi_app(application)
 
 
