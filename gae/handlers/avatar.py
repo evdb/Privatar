12fd5f9255592bd090236aa795ad0eee872cfa0b
Edmund von der Burg <evdb@mysociety.org>
2011-03-27 13:22:53 +0100

    Break up code Add memcaching of gravatar responses

--- gae/handlers/avatar.py
+++ gae/handlers/avatar.py
@@ -3,6 +3,7 @@ import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.api.urlfetch import fetch
+from google.appengine.api.memcache import Client
 
 from externals.privatar import privatar
 
@@ -16,23 +17,42 @@ class AvatarHandler(webapp.RequestHandler):
         # request the gravatar image and return it (caching as we go)
         gravatar_url = "http://www.gravatar.com/avatar/%s" % gravatar_md5
         
-        res = fetch( gravatar_url )
-        # logging.debug( res.headers )
-        
+        # serve the gravatar url
+        self.serve_gravatar_url( gravatar_url )
+
+
+    def serve_gravatar_url( self, gravatar_url ):
+
+        res = self.fetch_gravatar_url( gravatar_url )
+
         # set the headers and the content
-        self.response.headers['Content-Type'] = 'image=jpeg'
+        self.response.headers['Content-Type'] = 'image/jpeg'
         self.response.out.write(res.content)
+
+
+    def fetch_gravatar_url( self, gravatar_url ):
+        memcache = Client()
+        res = memcache.get( gravatar_url )
+        
+        if not res:
+            res = fetch( gravatar_url )
+            memcache.set( gravatar_url, res, time=3600 )
+        
+        return res
 
 
     def handle_exception(self, exception, debug_mode=False):
         super(AvatarHandler, self).handle_exception(exception, debug_mode)
         # return self.error(404)
 
+
     def get_gravatar_md5(self):
-        return '00000000000000000000000000000000'
-        
         # extract the privatar code from the url
         
-        # get the md5 we should be requesting
+        # get the site_key
         
+        # load the shared_secret dictionary for the site key
         
+        # decrypt md5
+                
+        return '00000000000000000000000000000000'
