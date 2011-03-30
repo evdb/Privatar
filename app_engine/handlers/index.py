5a55c6186b2613bcb0665b0d3a1b9be2a8ab640f
Edmund von der Burg <evdb@mysociety.org>
2011-03-30 21:28:47 +0100

    Generate web pages from templates

--- app_engine/handlers/index.py
+++ app_engine/handlers/index.py
@@ -1,9 +1,25 @@
+import os
+import logging
+
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
 
 class IndexHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        
+        # get the path and see if it can be made to match a template
+        request_path  = self.request.path[1:] or 'index'
+        template_path = 'templates/' + request_path + '.html'
 
+        logging.debug( template_path )
 
+        if not os.path.exists(template_path):
+            return self.error(404)
+                
+        self.response.out.write(
+            template.render( 
+                template_path,
+                {}
+            )
+        )
