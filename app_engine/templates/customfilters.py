d5523d69eff81ccacf2e18b2f10972b4bc0eac9d
Edmund von der Burg <evdb@mysociety.org>
2011-04-02 16:02:46 +0100

    Add filter to display privatar

---
+++ app_engine/templates/customfilters.py
@@ -0,0 +1,29 @@
+import logging
+
+from google.appengine.ext import webapp
+from externals.privatar import privatar
+ 
+CACHED_PRIVATAR = None
+
+def get_privatar():
+    global CACHED_PRIVATAR        
+    if not CACHED_PRIVATAR:
+        logging.debug( 'setting up privatar object' )
+        CACHED_PRIVATAR = privatar( 'privatar', 'FIXME' )
+        CACHED_PRIVATAR.http_base = ''
+                    
+    return CACHED_PRIVATAR
+
+
+def privatar_url_from_email(value):
+    p = get_privatar()
+    return p.url(email=value)
+
+def privatar_url_from_md5(value):
+    p = get_privatar()
+    return p.url(email=value)
+ 
+
+register = webapp.template.create_template_register()
+register.filter(privatar_url_from_email)
+register.filter(privatar_url_from_md5)
